4f60e357e5b283408e9a359f832057f99bbc68a0
Gabriel Luque <GabJL@users.noreply.github.com>
2022-09-19 14:29:26 +0200

    Update t2e01.print.py

--- clases/clase02/t2e01.print.py
+++ clases/clase02/t2e01.print.py
@@ -4,8 +4,8 @@ print("Hola Gabriel!", end="\n\n")
 # En este caso como por defecto el print añade un salto y en el texto pongo manualmente otro, tengo los dos saltos
 print("Este es mi primer programa en Python ... de este tema\n") 
 
-horas = 0
-minutos = 21
+horas: int = 0
+minutos: int = 21
 # En este caso los dos saltos los logro con 2 print que cada uno hace uno (en el siguiente no escribo nada, solo el salto).
 print("Han pasado", horas*3600 + minutos*60,"segundos desde el inicio de clase", end="\n\n")
 print()
