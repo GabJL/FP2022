d37d785e85b16a5e149e4aa7e0a8421936daf143
Gabriel Luque <GabJL@users.noreply.github.com>
2022-10-10 09:16:07 +0200

    Create p3e01.notas1.py

---
+++ prácticas/práctica03/p3e01.notas1.py
@@ -0,0 +1,11 @@
+MAX_NOTAS: int = 5
+
+print("Introduce", MAX_NOTAS,"notas:")
+
+aprobados: int = 0
+for i in range(MAX_NOTAS):
+  nota: int = float(input("Nota: "))
+  if nota >= 5:
+    aprobados += 1
+    
+print("Hay", aprobados, "alumnos aprobados")
