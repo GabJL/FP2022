c59db3774524da08516bc388429c183cc638b9ed
Gabriel Luque <GabJL@users.noreply.github.com>
2022-11-26 11:59:34 +0100

    Create variables.py

---
+++ clases/clase19/variables.py
@@ -0,0 +1,18 @@
+# Ejercicio 1 y 2
+
+Málaga: dict = {"enero": 12, "febrero": 12, "marzo": 14, "abril": 16, "mayo": 19, "junio": 23, "julio": 26, "agosto": 26,"septiembre": 23, "octubre": 19, "noviembre": 15, "diciembre": 13}
+
+Yakustk: dict = {"enero": -38, "febrero": -34, "marzo": -20, "abril": -5, "mayo": 8, "junio": 16, "julio": 20, "agosto": 15, "septiembre": 6, "octubre": -8, "noviembre": -27, "diciembre": -37}
+
+# Ejercicio 3
+Salud: dict = {
+  "Bioquímica estructural": [0.66, 0.61, 0.47, 0.64, 0.92, 0.92, 0.75], 
+  "Cálculo": [0.55, 0.52, 0.48, 0.29, 0.29, 0.06, 0.17], 
+  "Fundamentos de la programación": [0.58, 0.66, 0.53, 0.76, 0.7, 0.6, 0.49], 
+  "Física I": [0.23, 0.41, 0.38, 0.45, 0.37, 0.4, 0.56], 
+  "Álgebra lineal": [0.48, 0.44, 0.46, 0.54, 0.61, 0.41, 0.51]
+}
+
+
+# Ejercicio 4
+peli = "Spider-Man: No Way Home#Jon Wa#Tom Holland, Zendaya, Benedict Cumberbatch, Jacob Batalon#Action, Adventure, Fantasy#2021 8.3 148 804.75"
